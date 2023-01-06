652248a8de6c3e6a0263a823bb1d3c865718ba3c
Artem Bolshak <artem.bolshak@eosda.com>
2023-01-06 15:02:41 +0200

    Forecast application layout with scalable forecast-panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'forecast-app';
+export class AppComponent implements OnInit {
+  @ViewChild('forecastContainer', { read: ElementRef }) forecastContainer: ElementRef;
+
+  isInfoBlockScrolled$ = new BehaviorSubject<boolean>(false);
+
+  constructor() { }
+
+  @HostListener('window: resize')
+  onResize(): void {
+    const element = this.forecastContainer.nativeElement;
+    element.scrollHeight > element.clientHeight
+      ? this.isInfoBlockScrolled$.next(true)
+      : this.isInfoBlockScrolled$.next(false);
+  }
+
+  ngOnInit(): void {
+    console.log(this.isInfoBlockScrolled$.getValue());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ForecastContainerComponent } from './forecast-container/forecast-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForecastContainerComponent
   ],
   imports: [
     BrowserModule
