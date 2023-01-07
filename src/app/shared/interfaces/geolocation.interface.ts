209084df4acb4fe7312a1aa563ca0b20de7710da
Artem Bolshak <artem.bolshak@eosda.com>
2023-01-07 16:38:02 +0200

    Interfaces for forecast and geolocation

---
+++ src/app/shared/interfaces/geolocation.interface.ts
@@ -0,0 +1,12 @@
+export interface LngLat {
+  lon: number;
+  lat: number;
+}
+
+export interface City {
+  name: string;
+  country: string;
+  state: string;
+  lat: number;
+  lon: number;
+}
---
+++ src/app/shared/shared.constants.ts
@@ -0,0 +1,1 @@
+export const appId = 'a34fcc171dc45409dcb4d2f9202914ff';
